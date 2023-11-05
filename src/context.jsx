14d38309d6590fc6b7bcf54795104d2d911f5580
Benedcit <aliubenedict@gmail.com>
2023-11-05 23:20:23 +0100

    still working on reducer

--- src/context.jsx
+++ src/context.jsx
@@ -2,18 +2,29 @@ import { useReducer } from "react";
 import { useContext } from "react";
 import { createContext } from "react";
 import reducer from "./reducer";
+import cartItems from "./data";
 
 const AppContext = createContext();
 export const AppProvider = ({ children }) => {
+  const initialState = {
+    loading: false,
+    cart: new Map(cartItems.map((item) => [item.id, item])),
+  };
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-const initialState = {
-    loading: false, 
-    cart: []
-}
-    const [state,dispatch] = useReducer(reducer,initialState)
-  return <AppContext.Provider value={...state}>{children}</AppContext.Provider>;
+  const clearCart = () => {
+    dispatch({ type: "CLEAR_CART" });
+  };
+  const removeItem = (id) => {
+    dispatch({ type: "REMOVE_ITEM", payload: { id } });
+  };
+  return (
+    <AppContext.Provider value={{ ...state, clearCart, removeItem }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 export const useGlobalContext = () => {
-  useContext(AppContext);
+  return useContext(AppContext);
 };
--- src/reducer.jsx
+++ src/reducer.jsx
@@ -1,4 +1,19 @@
 const reducer = (state, action) => {
-  return state;
+  switch (action.type) {
+    case "CLEAR_CART":
+      return {
+        ...state,
+        cart: new Map(),
+      };
+    case "REMOVE_ITEM":
+      const newCart = new Map(state.cart);
+      newCart.delete(action.payload.id);
+      return {
+        ...state,
+        cart: newCart,
+      };
+    default:
+      return state;
+  }
 };
 export default reducer;
